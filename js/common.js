e63618f2758a8fe1eef2c3d2a52a712e5d32af07
Michael Tomkins <michft@gmail.com>
2014-09-16 11:32:58 +1000

    Final Submit. Took 2.5 hours with JS, Styling and debugging from scratch.

--- js/common.js
+++ js/common.js
@@ -10,24 +10,24 @@ function mychange(amount) {
 
     bar_value = bar_value.substring(0, bar_value.length - 2);
     bar_number = Number(bar_value) + amount;
-
+    if (bar_number > 0) {
+        bar_off =  100 / bar_number * 100;
+    }
 
     if (bar_number <= 0) {
-        document.getElementById(select_bars).style.width='0%';
-        document.getElementById(select_text).style.width='100%';
+        document.getElementById(select_bars).style.width='0.5%';
+        document.getElementById(select_text).style.width='20000%'; // Ugly but works on Firefox and linux.
         document.getElementById(select_text).innerHTML='0 %';
-        document.getElementById(select_bars).style.background='#00cc00'
+        document.getElementById(select_bars).style.background='#00cc00';
     } else if (bar_number <= 95) {
-        bar_off =  100 / bar_number * 100;
-        alert (bar_off);
         document.getElementById(select_bars).style.width=bar_number +'%';
-        document.getElementById(select_text).style.width='100%';
+        document.getElementById(select_text).style.width=bar_off + '%';
         document.getElementById(select_text).innerHTML=bar_number +' %';
-        document.getElementById(select_bars).style.background='#00cc00'
+        document.getElementById(select_bars).style.background='#00cc00';
     } else {
         document.getElementById(select_bars).style.width='100%';
         document.getElementById(select_text).style.width='100%';
         document.getElementById(select_text).innerHTML=bar_number +' %';
-        document.getElementById(select_bars).style.background='#cc0000'
+        document.getElementById(select_bars).style.background='#cc0000';
     } // */
 }
